1a3bae85f81be8b53321a173e2676e8c3d40e967
egrimstad <egrimstad95@gmail.com>
2017-04-29 17:23:10 +0200

    Add time control

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="hello">
-    <div v-for="person in persons">
-      {{person.name}}: {{getBAC(person)}}
-    </div>
+  <h1>{{date(time).toDateString()}}</h1>
+  <h2>{{date(time).toLocaleTimeString()}}</h2>
+  <div>
+    Samlet promille: {{getTotalBAC()}}
+  </div>
+  <div>
+    Gjennomsnittlig: {{getAverageBAC()}}
+  </div>
+    <input type="button" v-on:click="play()" value="play/pause">
     <input type="range" v-model="time" min="1493460000" max="1493478000"/>
-    {{date(time)}}
+    Speed:
+    <input type="button" v-on:click="speed+=10; play();play();" value="+">
+    <input type="button" v-on:click="speed-=10; play();play();" value="-">
+    
+    <h2>Timeline</h2>
     <div v-for="trans in getTransactions()">
-      {{date(trans.time)}}: {{getPerson(trans.person).name}} bought {{trans.item}}
+      {{date(trans.time).toLocaleTimeString()}}: {{getPerson(trans.person).name}} bought {{trans.item}}
     </div>
   </div>
 </template>
@@ -17,8 +27,8 @@ export default {
   // {{date(trans.data)}}: {{persons[trans.person].name}} bought {{trans.item}}
   name: 'hello',
 
-  data () {
-    function compTrans (a, b) {
+  methods: {
+    compTrans: function (a, b) {
       if (a.time < b.time) {
         return -1
       }
@@ -27,56 +37,95 @@ export default {
       }
 
       return 0
-    }
+    },
 
+    date: function (epoch) { return new Date(epoch * 1000) },
+
+    play: function () {
+      if (this.playing) {
+        clearInterval(this.timer)
+      } else {
+        this.timer = setInterval(this.update, 1000 / this.speed)
+      }
+
+      this.playing = !this.playing
+    },
+
+    update: function () {
+      this.time++
+    },
+
+    getPerson: function (id) {
+      var p = null
+      this.persons.forEach(function (person) {
+        if (person.id === id) {
+          p = person
+        }
+      })
+      return p
+    },
+
+    getTransactions: function () {
+      var trans = []
+      for (var i = 0; i < this.transactions.length; i++) {
+        if (this.transactions[i].time <= this.time) {
+          trans.push(this.transactions[i])
+        }
+      }
+      trans.sort(this.compTrans)
+      trans.reverse()
+
+      return trans
+    },
+    getTransactionsForPerson: function (person) {
+      var trans = []
+      for (var i = 0; i < this.transactions.length; i++) {
+        if (this.transactions[i].time <= this.time && this.transactions[i].person === person.id) {
+          trans.push(this.transactions[i])
+        }
+      }
+      trans.sort(this.compTrans)
+
+      return trans
+    },
+
+    getBAC: function (person) {
+      var trans = this.getTransactionsForPerson(person)
+      if (trans.length === 0) {
+        return 0
+      }
+      var startTime = trans[0].time
+      var hoursSinceStart = (this.time - startTime) / 3600
+
+      var bac = 12 * trans.length / (person.weight * 0.6) - 0.15 * hoursSinceStart
+
+      if (bac < 0) {
+        bac = 0
+      }
+
+      return Math.round(bac * 100) / 100
+    },
+
+    getTotalBAC: function () {
+      var bac = 0
+      var t = this
+      this.persons.forEach(function (person) {
+        bac += t.getBAC(person)
+      })
+      return bac
+    },
+    getAverageBAC: function () {
+      return this.getTotalBAC() / this.persons.length
+    }
+  },
+
+  data () {
     return {
       msg: 'Welcome to Your Vue.js App',
       time: 1493460000,
-      date: function (epoch) { return new Date(epoch * 1000) },
-
-      getTransactions: function () {
-        var trans = []
-        for (var i = 0; i < this.transactions.length; i++) {
-          if (this.transactions[i].time <= this.time) {
-            trans.push(this.transactions[i])
-          }
-        }
-        trans.sort(compTrans)
-
-        return trans
-      },
-      getTransactionsForPerson: function (person) {
-        var trans = []
-        for (var i = 0; i < this.transactions.length; i++) {
-          if (this.transactions[i].time <= this.time && this.transactions[i].person === person.id) {
-            trans.push(this.transactions[i])
-          }
-        }
-        trans.sort(compTrans)
-
-        return trans
-      },
-
-      getBAC: function (person) {
-        var trans = this.getTransactionsForPerson(person)
-        if (trans.length === 0) {
-          return 0
-        }
-        var startTime = trans[0].time
-        var hoursSinceStart = (this.time - startTime) / 3600
-
-        var bac = 12 * trans.length / (person.weight * 0.6) - 0.15 * hoursSinceStart
-
-        return Math.round(bac * 100) / 100
-      },
-
-      getPerson: function (id) {
-        for (var i = 0; i < this.persons.length; i++) {
-          if (this.persons[i].id === id) {
-            return this.persons[i]
-          }
-        }
-      },
+      timer: null,
+      speed: 10,
+      playing: false,
 
       persons: [
         {
